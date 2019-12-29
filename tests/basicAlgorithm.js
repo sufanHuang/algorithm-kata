585f7fe961e98e39ce26afcbafe9cf49c8e901f5
sufanHuang <sufanhk@gmail.com>
2019-12-28 21:04:55 -0600

    Add convertCelsiusToFahrenheit function and tests

--- tests/basicAlgorithm.js
+++ tests/basicAlgorithm.js
@@ -1,5 +1,37 @@
 const expect = require('chai').expect
 const Chance = require('chance')
+const _ = require('lodash')
 
 const chance = new Chance
 const modulePath = '../functions/basicAlgorithm'
+
+describe('basicAlgorithm functionality', () => {
+
+    let api = require(modulePath)
+
+    describe('convertCelsiusToFahrenheit functionality', () => {
+
+        it('should return a number', () => {
+            let value = chance.integer()
+            let result = api.convertCelsiusToFahrenheit(value)
+
+            expect(_.isNumber(result)).to.be.true
+        });
+
+        it('should convert celsius to fahrenheit', () => {
+            let knownMapping = {
+                '20': 68,
+                '10': 50,
+                '0': 32,
+                '-30': -22,
+                '30': 86
+            }
+
+            let celciusValues = _.keys(knownMapping)
+            _.each(celciusValues, (value) => {
+                let result = api.convertCelsiusToFahrenheit(parseInt(value))
+                expect(result).to.equal(knownMapping[value])
+            })
+        })
+    });
+});
